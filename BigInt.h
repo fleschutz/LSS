3da8333c1c67b22ef05894a1e1e713a889542fa1
Markus Fleschutz <markus.fleschutz@gmail.com>
2023-09-06 20:57:34 +0200

    Add BigInt.h

---
+++ BigInt.h
@@ -0,0 +1,64 @@
+
+typedef __int128_t     BigInt;
+
+// Returns the given string as BigInt.
+BigInt BigIntFromString(const char *str)
+{
+	BigInt sign = 1, value = 0;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	for (size_t i = 0; i < strlen(str); ++i)
+		value = (value * 10) + (str[i] - '0');
+	return sign * value;
+}
+
+// Returns the given power of 10 number as BigInt.
+BigInt BigIntFromPowerOf10(int exponent)
+{
+	BigInt result = 1;
+	for (int i = 0; i < exponent; ++i)
+		result *= (BigInt)10;
+	return result;
+}
+
+// Prints the given big number on the console.
+void printBigInt(BigInt n)
+{
+	char buf[80] = {}, *ptr = buf + sizeof(buf) - 1;  // start at the end
+	if (n < 0)
+	{
+		n = -n;
+                fputc('-', stdout);
+	}
+	do
+	{
+		*--ptr = "0123456789"[n % 10];    // save last digit
+		n /= 10;                // drop it
+	} while (n);
+	fprintf(stdout, "%s", ptr);
+}
+
+// Special printf() to support "%B" for BigInt variables.
+void printLine(const char* formatString, ...)
+{
+	va_list ptr;
+	va_start(ptr, formatString);
+
+	for (int i = 0; formatString[i] != '\0'; ++i)
+	{
+		if (formatString[i] == '%') {
+			if (formatString[++i] == 'B')
+				printBigInt(va_arg(ptr, BigInt));
+		}
+		else
+                	fputc(formatString[i], stdout);
+	}
+	fprintf(stdout, "\n");
+	fflush(stdout); // to disable buffering
+	va_end(ptr);
+}
--- main.c
+++ main.c
@@ -4,76 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "BigInt.h" // BigInt datatype and support functions
 
 #define N_MIN               0 // minimum desired value for n
 #define N_MAX            1000 // maximum desired value for n
 #define XYZ_MIN             0 // minimum value for x,y,z to use
 #define XYZ_MAX        100000 // maximum value for x,y,z to use
 #define CSV_OUTPUT          0 // CSV output desired, else text output
-
-typedef __int128_t     BigInt;
-
-// Returns the given string as BigInt.
-BigInt BigIntFromString(const char *str)
-{
-	BigInt sign = 1, value = 0;
-	if (*str == '+')
-		str++;
-	else if (*str == '-')
-	{
-		sign = -1;
-		str++;
-	}
-	for (size_t i = 0; i < strlen(str); ++i)
-		value = (value * 10) + (str[i] - '0');
-	return sign * value;
-}
-
-// Returns the given power of 10 number as BigInt.
-BigInt BigIntFromPowerOf10(int exponent)
-{
-	BigInt result = 1;
-	for (int i = 0; i < exponent; ++i)
-		result *= (BigInt)10;
-	return result;
-}
-
-// Prints the given big number on the console.
-void printBigInt(BigInt n)
-{
-	char buf[80] = {}, *ptr = buf + sizeof(buf) - 1;  // start at the end
-	if (n < 0)
-	{
-		n = -n;
-                fputc('-', stdout);
-	}
-	do
-	{
-		*--ptr = "0123456789"[n % 10];    // save last digit
-		n /= 10;                // drop it
-	} while (n);
-	fprintf(stdout, "%s", ptr);
-}
-
-// Special printf() to support "%B" for BigInt variables.
-void printLine(const char* formatString, ...)
-{
-	va_list ptr;
-	va_start(ptr, formatString);
-
-	for (int i = 0; formatString[i] != '\0'; ++i)
-	{
-		if (formatString[i] == '%') {
-			if (formatString[++i] == 'B')
-				printBigInt(va_arg(ptr, BigInt));
-		}
-		else
-                	fputc(formatString[i], stdout);
-	}
-	fprintf(stdout, "\n");
-	fflush(stdout); // to disable buffering
-	va_end(ptr);
-}
 
 // Provides pre-calculated cube numbers for performance. Afterward, use 'cubeNumbers[x]' instead of 'x*x*x'.
 BigInt cubeNumbers[XYZ_MAX + 1];
